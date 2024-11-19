65c1c8734625172217cb4b2f354b91d981f3181e
Peter Rodriguez <peter.rodriguez@tia.com.ec>
2024-11-19 14:30:57 -0500

    BackEnd

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,8 +1,23 @@
 
 const { io } = require('../index');
+const Bands = require('../models/bands')
+const Band = require('../models/band')
+
+const bands = new Bands();
+
+bands.addBand( new Band ('Rock'));
+bands.addBand( new Band ('Queen'));
+bands.addBand( new Band ('Metallica'));
+bands.addBand( new Band ('Banda'));
+
+
 
 io.on('connection', client => {
     console.log("Cliente conectado")
+
+    client.emit('active-bands', bands.getBands() );
+
+
     client.on('disconnect', () => { 
         console.log("Cliente desconectado")
     });
@@ -14,4 +29,26 @@ io.on('connection', client => {
 
     });
 
+    client.on('emitir-mensaje', (payload) => {
+        console.log(payload);
+        client.broadcast.emit('emitir-mensaje', payload );
+
+    });
+
+    client.on('vote-band', (payload) => {
+        //console.log(payload);
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('add-band', (payload) => {
+        const newBand = new Band (payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands() );
+    });
+
+    client.on('delete-band', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands() );
+    });
 });
